3e87c4bae1978bf5615c0ec9cb077ba2b3ed4767
Tiago Martinho <t.martinho@icloud.com>
2022-02-07 16:09:24 +0800

    Initial Commit

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Tiago Martinho on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
